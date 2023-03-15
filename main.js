19110e355a548a7032726906f042a8956efbf822
JhonnSantiagoBernalCampus <jhonbernalcampus@gmail.com>
2023-03-15 09:22:51 -0500

    feat: :zap: Ingresando datos

---
+++ main.js
@@ -0,0 +1,33 @@
+let miFormularioCampus = document.querySelector("#miFormularioCampus");
+let miFormularioPersonas = document.querySelector("#miFormularioPersonas");
+let campus = {};
+
+
+miFormularioCampus.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target))
+    campus[`${data.nombreSede}`] = {Camper: [], Trainers: []};
+    listaSedes();
+    miFormularioCampus.reset();
+})
+
+let listaSedes = ()=>{
+    let opciones = document.querySelector("[name='sede']");
+    opciones.innerHTML = null;
+    for (let [val, id] of Object.entries(campus)) {
+        opciones.insertAdjacentHTML("beforeend", `
+            <option value="${val}">${val}</option>
+        `);
+    }
+}
+
+miFormularioPersonas.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede = data.sede;
+    delete data.sede;
+    campus[`${sede}`]["Camper"].unshift(data);
+    console.log(campus);
+    miFormularioPersonas.reset();
+})
